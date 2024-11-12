f81d299af2bb1a559b6cc18ed19d576063f87e41
7kax <1196125991@qq.com>
2024-11-13 00:15:05 +0800

    修复编码功能

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import './style.css'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import './style.css';
 
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
+
+import * as buffer from 'buffer';
+
+if (typeof (window as any).Buffer === 'undefined') {
+    (window as any).Buffer = buffer.Buffer;
+}
 
 const app = createApp(App)
 const pinia = createPinia()
