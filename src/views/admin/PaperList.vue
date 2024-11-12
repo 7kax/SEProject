c9240cd99264b8e4887b50c23a4bdace7b4ab711
7kax <1196125991@qq.com>
2024-11-12 20:09:31 +0800

    调整格式

--- src/views/admin/PaperList.vue
+++ src/views/admin/PaperList.vue
@@ -3,9 +3,9 @@
     <el-container>
         <el-header>论文列表</el-header>
         <el-main>
-            <el-table :data="papers" style="width: 100%">
-                <el-table-column prop="title" label="论文标题" width="300"></el-table-column>
-                <el-table-column label="作者">
+            <el-table :data="papers" fit>
+                <el-table-column prop="title" label="论文标题"></el-table-column>
+                <el-table-column prop="author" label="作者">
                     <template #default="scope">
                         <span
                             v-for="(author, index) in [...scope.row.firstAuthor, ...scope.row.secondAuthor, ...scope.row.thirdAuthor]"
@@ -14,30 +14,35 @@
                         </span>
                     </template>
                 </el-table-column>
-                <el-table-column prop="CCF" label="CCF分级" width="150"></el-table-column>
+                <el-table-column prop="CCF" label="CCF分级" width="120"></el-table-column>
             </el-table>
         </el-main>
     </el-container>
 </template>
 
 <script setup lang="ts">
+import { errorAlert } from '@/utils/alert';
 import { getWithToken } from '@/utils/request';
 import { onMounted, ref } from 'vue';
 
 // 组件挂载时, 向后端请求论文数据
 const papers = ref<Paper[]>([]);
 const refreshPapers = () => {
-    const id = JSON.parse(localStorage.getItem('user') as string).id;
-    const url = `/api/papers/${id}`;
+    const url = `/api/papers`;
     const token = localStorage.getItem('token') as string;
     getWithToken(url, token).then((res) => {
-        papers.value = res.data.papers;
-    });
+        console.log(res);
+        if (res.status === 200) {
+            papers.value = res.data.papers;
 
-    // 删去papers中status不为"审核通过"的元素
-    papers.value = papers.value.filter((paper) => paper.status === Status.Approve);
+            // 删去papers中status不为"审核通过"的元素
+            papers.value = papers.value.filter((paper) => paper.status === Status.Approve);
+        }
+    }).catch((err) => {
+        errorAlert(err.response.data.message);
+    });
 };
-onMounted(async () => {
+onMounted(() => {
     refreshPapers();
 });
 
--- src/views/user/PaperBoard.vue
+++ src/views/user/PaperBoard.vue
@@ -1,1 +1,6 @@
 <!-- 用户论文看板 -->
+<template></template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
