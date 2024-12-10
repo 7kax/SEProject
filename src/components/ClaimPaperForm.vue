f5e97e7efd68ea3b0b7e410fb228ba2cc451f5fa
Jiang Xinyi <21307130015@m.fudan.edu.cn>
2024-12-10 21:48:29 +0800

    修改认领论文组件

--- src/components/ClaimPaperForm.vue
+++ src/components/ClaimPaperForm.vue
@@ -1,7 +1,7 @@
 <!-- 用户认领论文的表单 -->
 <template>
-    <!-- <h1>论文认领</h1>
-    <div>请输入查询的DOI<input v-model="DOI"><button @click="queryPaperWithDOI">查找</button></div>
+    <h1>论文认领</h1>
+    <div>请输入查询的DOI<input v-model="DOI"><el-button @click="queryPaperWithDOI">查找</el-button></div>
     <el-table :data="papers" style="width: 100%">
         <el-table-column prop="DOI" label="DOI" width="120" sortable />
         <el-table-column prop="title" label="标题" width="120" sortable />
@@ -15,53 +15,73 @@
                 <el-button type="primary" @click="claimPaper(scope.row)">申请认领</el-button>
             </template>
 </el-table-column>
-</el-table> -->
+</el-table>
 </template>
 
 <script lang="ts" setup name="ClaimPaperForm">
-// import { ref } from 'vue'
-// import { getWithToken, postWithToken } from '@/utils/request';
-// import { useAuthStore } from '@/stores/auth';
-// import { errorAlert, successAlert } from '@/utils/alert';
-// import { useRouter } from 'vue-router';
+import { ref } from 'vue'
+import { fetchPaper } from '@/requests/paper';
+import { addClaim } from '@/requests/application';
+import { useAuthStore } from '@/stores/auth';
+import { errorAlert, successAlert } from '@/utils/alert';
+import { useRouter } from 'vue-router';
 
-// const authStore = useAuthStore();
-// const router = useRouter();
+const authStore = useAuthStore();
+const router = useRouter();
 
-// const DOI = ref("")
-// const user = authStore.user;
-// const papers = ref<Array<Paper>>(new Array<Paper>)
+const DOI = ref("")
+const user = authStore.user;
+const papers = ref<Paper[]>([])
 
 
-// const queryPaperWithDOI = () => {
-//     const token = user!.token;
-//     getWithToken('/api/papers/${DOI.value}', token).then((res) => {
-//         console.log(res);
-//         if (res.status === 200) {
-//             papers.value = res.data.papers
-//         } else if (res.status === 401) {
-//             errorAlert('请先登录');
-//             router.push('/login');
-//         } else {
-//             errorAlert('该论文不存在');
-//         }
-//     });
-// }
+const queryPaperWithDOI = () => {
+    const params = {
+        id: user?.id || "",
+        doi: DOI.value,
+    };
 
-// const claimPaper = (paper: Paper) => {
-//     const url = '/api/papers/request/claim'
-//     const token = user!.token;
-//     const data = JSON.stringify({ doi: paper.DOI })
-//     postWithToken(url, data, token).then((res) => {
-//         console.log(res);
-//         if (res.status === 200) {
-//             successAlert('申请成功，请在用户论文列表查看审批进程')
-//         } else if (res.status === 401) {
-//             errorAlert('请先登录');
-//             router.push('/login');
-//         } else {
-//             errorAlert('论文信息错误，请联系管理员');
-//         }
-//     })
-// }
+    fetchPaper(params).then((_res) => {
+        papers.value = _res.papers;
+    }).catch((err) => {
+        errorAlert(err.response.data.message);
+    });
+}
+
+const claimPaper = (paper: Paper) => {
+    // const url = '/api/papers/request/claim'
+    // const token = user!.token;
+    // const data = JSON.stringify({ doi: paper.DOI })
+    // postWithToken(url, data, token).then((res) => {
+    //     console.log(res);
+    //     if (res.status === 200) {
+    //         successAlert('申请成功，请在用户论文列表查看审批进程')
+    //     } else if (res.status === 401) {
+    //         errorAlert('请先登录');
+    //         router.push('/login');
+    //     } else {
+    //         errorAlert('论文信息错误，请联系管理员');
+    //     }
+    // })
+    const params = {
+        doi: paper.DOI
+    }
+    addClaim(params).then((_res) => {
+        if (_res.status === 200) {
+            successAlert('申请成功，请在用户论文列表查看审批进程');
+            router.push("/user/paper_list");
+        } else if (_res.status === 401) {
+            errorAlert('请先登录');
+            router.push('/login');
+        } else {
+            errorAlert('论文信息错误，请联系管理员');
+        }
+    }).catch((err) => {
+        errorAlert(err.response.data.message);
+    });
+}
+
 </script>
+
+<style scoped>
+
+</style>
